dbdecf35948aafbc242c73577989ed12c46906f4
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-07-01 17:20:15 +0200

    Test env

--- travis.sh
+++ travis.sh
@@ -6,11 +6,33 @@ function installTravisTools {
   curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v2.1/install.sh | bash
 }
 
+function java_run_its {
+  reset_ruby
+  install_jars
+
+  if [ "$1" == "IT-DEV" ]; then
+    VERSION="DEV"
+
+    build_green "SonarSource/sonarqube" "master"
+  else
+    VERSION="5.1.1"
+
+    echo "Downloading latest SonarQube release [$1]..."
+    mkdir -p ~/.m2/repository/org/codehaus/sonar/sonar-application/$VERSION
+    curl -sSL http://downloads.sonarsource.com/sonarqube/sonarqube-$VERSION.zip -o ~/.m2/repository/org/codehaus/sonar/sonar-application/$VERSION/sonar-application-$VERSION.zip
+  fi
+
+  cd its/plugin
+  mvn -Dmaven.test.redirectTestOutputToFile=false -Dsonar.runtimeVersion="$VERSION" test -Dtest=com.sonar.it.java.suite.$1
+
+  unset VERSION
+}
+
 if [ "$TESTS" == "CI" ]; then
   mvn verify -B -e -V
 else
   mvn install -DskipTests=true
 
   installTravisTools
-  travis_run_its "${TESTS}"
+  java_run_its "${TESTS}" ${IT_TEST_CLASS}
 fi
